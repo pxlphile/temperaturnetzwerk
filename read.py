fa3fc7897bc9729aad140c696121f446b2c05b99
Pi <Pi@pi>
2016-06-09 14:33:10 +0000

    Temperatur und Durchschnitt in Plot eingebunden

--- read.py
+++ read.py
@@ -8,28 +8,51 @@ import os
 dbConnection = None
 dbCursor = None
 DELIMITER = ";"
+rollingAverage = None
 
+def initAverage(size):
+	rollAvg = []
+	for i in range(size):
+		rollAvg.append(0.0)
+	return rollAvg
 
 def main():
+	global rollingAverage
+	rollingAverage = initAverage(10)
+	
 	openDb()
 	dbResult = readFromDb()
 	
 	with open("datefile.txt","w") as outputFile:
 		for row in dbResult:
+			updateAverage(row[1])
 			writeDataSet(row, outputFile)
 	
 	closeDb()
 
 def readFromDb():
-	#dbCursor.execute("delete from temperatur")
 	result = dbCursor.execute("select * from temperatur")
-	#dbConnection.commit()
 	return result
+
+def updateAverage(temp):
+	global rollingAverage
+	rollingAverage.pop(0)
+	rollingAverage.append(temp)
 
 def writeDataSet(row, outputFile):
 		outputFile.write('"' + row[0] +'"')
 		outputFile.write(DELIMITER)
-		outputFile.write(str(row[1]) + "\n")
+		outputFile.write(str(row[1]))
+		outputFile.write(DELIMITER)
+		average = calculateAverage()
+		outputFile.write(str(average) + "\n")
+
+def calculateAverage():
+	global rollingAverage
+	result = 0.0
+	for roll in rollingAverage:
+		result += roll
+	return result / len(rollingAverage)
 
 def openDb():
 	global dbConnection
