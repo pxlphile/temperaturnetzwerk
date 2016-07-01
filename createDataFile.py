52c6fc55a6de12401286ae0cc0f5e3a543cf8c46
Pi <Pi@pi>
2016-07-01 08:33:42 +0200

    Durchschnitt-Feature entfernt
    Der Durchschnitt nährte sich stets und sehr stark der temperatur-Kurve an
    und bietet keinen Mehrwert

--- createDataFile.py
+++ createDataFile.py
@@ -10,49 +10,24 @@ dbCursor = None
 DELIMITER = ";"
 rollingAverage = None
 
-def initAverage(size):
-	rollAvg = []
-	for i in range(size):
-		rollAvg.append(0.0)
-	return rollAvg
-
 def main():
-	global rollingAverage
-	rollingAverage = initAverage(10)
-	
 	openDb()
 	dbResult = readFromDb()
 	
 	with open("datefile.txt","w") as outputFile:
 		for row in dbResult:
-			updateAverage(row[1])
 			writeDataSet(row, outputFile)
 	
 	closeDb()
 
 def readFromDb():
-	result = dbCursor.execute("select datum,temp from temperatur")
-	return result
-
-def updateAverage(temp):
-	global rollingAverage
-	rollingAverage.pop(0)
-	rollingAverage.append(temp)
+	return dbCursor.execute("select datum,temp from temperatur")
 
 def writeDataSet(row, outputFile):
 		outputFile.write('"' + row[0] +'"')
 		outputFile.write(DELIMITER)
 		outputFile.write(str(row[1]))
-		outputFile.write(DELIMITER)
-		average = calculateAverage()
-		outputFile.write(str(average) + "\n")
-
-def calculateAverage():
-	global rollingAverage
-	result = 0.0
-	for roll in rollingAverage:
-		result += roll
-	return result / len(rollingAverage)
+		outputFile.write("\n")
 
 def openDb():
 	global dbConnection
